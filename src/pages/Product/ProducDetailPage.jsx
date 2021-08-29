fa2417f0253fe7f29337ef84dc0ad883efe87557
Rafi Fuady <rafifuady@gmail.com>
2021-08-29 14:54:42 +0700

    fix: product detail

--- src/pages/Product/ProducDetailPage.jsx
+++ src/pages/Product/ProducDetailPage.jsx
@@ -1,9 +1,153 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Button,
+  ButtonBase,
+  Card,
+  CardMedia,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import ShareIcon from "@material-ui/icons/Share";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory, useParams } from "react-router";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import FavoriteOutlinedIcon from "@material-ui/icons/FavoriteOutlined";
+import { productsActions } from "../../_redux/action/product.action";
+
+const styled = makeStyles((theme) => ({
+  media: {
+    height: 150,
+  },
+  cardTitle: {
+    background: "#F7F6F2",
+  },
+  card: {
+    border: "2px",
+    outline: "10px",
+  },
+  headerIcon: {
+    padding: theme.spacing(2, 2),
+    color: "white",
+  },
+  headerRow: {
+    backgroundColor: "#4B6587",
+    margin: theme.spacing(0, 0, 2),
+  },
+  titleRow: {
+    // backgroundColor: "#4B6587",
+    padding: theme.spacing(2),
+    // margin: theme.spacing(0,2),
+  },
+  checkoutRow: {
+    // padding: theme.spacing(2,0),
+    margin: theme.spacing(0, 0, 10, 0),
+    zIndex: "5",
+  },
+  checkoutItem: {
+    margin: theme.spacing(0, 2),
+    width: "40vw",
+  },
+  wrapper: {
+    backgroundColor: "#F0E5CF",
+    textAlign: "center",
+    minHeight: "80vh",
+  },
+}));
+
+const filterProduct = (products, id) => {
+  return products.filter((product) => {
+    const productId = product.id.toLowerCase();
+    return productId.includes(id);
+  });
+};
 
 export function ProducDetailPage() {
+  const classes = styled();
+  const { id } = useParams();
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const { product, productBought } = useSelector((state) => state.product);
+
+  const filteredProduct = filterProduct(product, id);
+
   return (
     <>
-      ini detail page
+      {filteredProduct[0] && (
+        <>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            className={classes.headerRow}
+          >
+            <ButtonBase
+              className={classes.headerIcon}
+              children={<ArrowBackIcon />}
+              onClick={() => history.goBack()}
+            />
+            <ButtonBase
+              className={classes.headerIcon}
+              children={<ShareIcon onClick={() => alert("Shared !")} />}
+            />
+          </Grid>
+          <Card>
+            <CardMedia
+              className={classes.media}
+              image={filteredProduct[0].imageUrl}
+            />
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-between"
+              className={classes.titleRow}
+            >
+              <Typography variant="h4">{filteredProduct[0].title}</Typography>
+              <Button>
+                {filteredProduct[0].loved === 1 && (
+                  <FavoriteOutlinedIcon
+                    onClick={() => alert("Remove from Wishlist")}
+                  />
+                )}
+                {filteredProduct[0].loved === 0 && (
+                  <FavoriteBorderIcon
+                    onClick={() => alert("added To Wishlist")}
+                  />
+                )}
+              </Button>
+            </Grid>
+          </Card>
+          <Box my={3} px={3}>
+            {filteredProduct[0].description}
+          </Box>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            className={classes.checkoutRow}
+            overflow="visible"
+          >
+            <Typography variant="h4" className={classes.checkoutItem}>
+              {filteredProduct[0].price}
+            </Typography>
+            <Button
+              className={classes.checkoutItem}
+              variant="contained"
+              color="primary"
+              onClick={() =>
+                dispatch(
+                  productsActions.checkout(filteredProduct[0], productBought)
+                )
+              }
+            >
+              <Typography variant="h4">Buy</Typography>
+            </Button>
+          </Grid>
+        </>
+      )}
     </>
-  )
+  );
 }
